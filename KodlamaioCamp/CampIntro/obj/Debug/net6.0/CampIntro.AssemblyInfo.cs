8609679fa2d3a9504427b3a815386c6b27348cb1
pinareray <pinareray3@gmail.com>
2024-02-11 17:56:41 +0300

    feat: add gitignore file

--- KodlamaioCamp/CampIntro/obj/Debug/net6.0/CampIntro.AssemblyInfo.cs
+++ KodlamaioCamp/CampIntro/obj/Debug/net6.0/CampIntro.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CampIntro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af9308f034e3f4b696754cc7a469ed1a5089a934")]
 [assembly: System.Reflection.AssemblyProductAttribute("CampIntro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CampIntro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
